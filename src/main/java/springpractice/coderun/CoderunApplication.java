86582ec09aca364554b82a8c5aa18b021ecbf208
ItzRainingTacos <163113723+aLkdh@users.noreply.github.com>
2025-05-15 20:18:46 +0900

    Initial commit

---
+++ src/main/java/springpractice/coderun/CoderunApplication.java
@@ -0,0 +1,13 @@
+package springpractice.coderun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderunApplication.class, args);
+    }
+
+}
