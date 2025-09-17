4723311828ade4b2ac667997114d3fe9d4f1dbde
ItzRainingTacos <163113723+aLkdh@users.noreply.github.com>
2025-09-17 12:42:08 +0900

    fix :: language

--- src/main/java/com/sanchae/coderun/global/config/SecurityConfig.java
+++ src/main/java/com/sanchae/coderun/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -50,6 +51,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api-docs").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 )
 
                 .sessionManagement((session) ->
