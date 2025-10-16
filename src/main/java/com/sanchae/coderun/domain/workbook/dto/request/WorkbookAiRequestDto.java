d2343e4397ae0b256c4ba22622cf6a89b9870d54
ItzRainingTacos <163113723+aLkdh@users.noreply.github.com>
2025-10-16 17:32:40 +0900

    fix :: fixed workbookCreateRequestDto

--- src/main/java/com/sanchae/coderun/domain/workbook/dto/request/WorkbookAiRequestDto.java
+++ src/main/java/com/sanchae/coderun/domain/workbook/dto/request/WorkbookAiRequestDto.java
@@ -2,11 +2,15 @@ package com.sanchae.coderun.domain.workbook.dto.request;
 
 import com.sanchae.coderun.domain.practice.entity.PracticeType;
 import com.sanchae.coderun.domain.workbook.entity.WorkbookProblemsCount;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkbookAiRequestDto {
 
     private Long workbookLanguageId;
