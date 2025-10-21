a3ac3b12c767b78c31f929918900a11fd66e4314
ItzRainingTacos <163113723+aLkdh@users.noreply.github.com>
2025-10-22 00:45:37 +0900

    fix :: rollback

--- src/main/java/com/sanchae/coderun/global/config/SecurityConfig.java
+++ src/main/java/com/sanchae/coderun/global/config/SecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -36,8 +38,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedOrigin("https://api.coderun.site");
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
