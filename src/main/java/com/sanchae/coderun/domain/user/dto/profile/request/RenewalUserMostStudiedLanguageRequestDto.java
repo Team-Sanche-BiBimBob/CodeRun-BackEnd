cba1198a848478cb31b0574365420a6374a3a44f
ItzRainingTacos <163113723+aLkdh@users.noreply.github.com>
2025-10-23 00:36:06 +0900

    fix :: profile patch

--- src/main/java/com/sanchae/coderun/domain/user/dto/profile/request/RenewalUserMostStudiedLanguageRequestDto.java
+++ src/main/java/com/sanchae/coderun/domain/user/dto/profile/request/RenewalUserMostStudiedLanguageRequestDto.java
@@ -1,12 +1,10 @@
 package com.sanchae.coderun.domain.user.dto.profile.request;
 
-import com.sanchae.coderun.domain.language.entity.Language;
 import lombok.Getter;
 
 @Getter
 public class RenewalUserMostStudiedLanguageRequestDto {
-    private Long id;
-    private Language mostStudiedLanguage;
+    private Long mostStudiedLanguageId;
     private Long mostStudiedLanguageProgress;
     private Long mostStudiedLanguageScore;
 }
--- src/main/java/com/sanchae/coderun/domain/user/dto/profile/request/RenewalUserRecentlyStudiedLanguageRequestDto.java
+++ src/main/java/com/sanchae/coderun/domain/user/dto/profile/request/RenewalUserRecentlyStudiedLanguageRequestDto.java
@@ -1,12 +1,10 @@
 package com.sanchae.coderun.domain.user.dto.profile.request;
 
-import com.sanchae.coderun.domain.language.entity.Language;
 import lombok.Getter;
 
 @Getter
 public class RenewalUserRecentlyStudiedLanguageRequestDto {
-    private Long id;
-    private Language recentlyStudiedLanguage;
+    private Long recentlyStudiedLanguageId;
     private Long recentlyStudiedLanguageProgress;
     private Long recentlyStudiedLanguageScore;
 }
